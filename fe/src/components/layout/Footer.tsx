89c235d984d143eaf4537227ba0455372895cd91
7066 <festeringwound@proton.me>
2024-03-26 20:19:19 +0800

    Added

---
+++ fe/src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+      <div className="w-full h-[50vh] bg-[#18BA51] flex justify-center items-center">
+        <h1>This is a placeholder for the footer.</h1>
+      </div>
+    );
+  }
