54dffe9b8c57c017870ac3d7b3a362d9a0316f3f
7066 <festeringwound@proton.me>
2024-03-27 20:02:52 +0800

    todo: do maps.

--- fe/src/pages/deliveryRange.tsx
+++ fe/src/pages/deliveryRange.tsx
@@ -10,38 +10,6 @@ export default function map() {
     "Хоймор хотхон",
     "Хоймор хотхон",
   ]);
-  // Initialize and add the map
-  let map;
-  async function initMap(): Promise<void> {
-    // The location of Uluru
-    const position = { lat: -25.344, lng: 131.031 };
-
-    // Request needed libraries.
-    //@ts-ignore
-    const { Map } = (await google.maps.importLibrary(
-      "maps"
-    )) as google.maps.MapsLibrary;
-    const { AdvancedMarkerElement } = (await google.maps.importLibrary(
-      "marker"
-    )) as google.maps.MarkerLibrary;
-
-    // The map, centered at Uluru
-    map = new Map(document.getElementById("map") as HTMLElement, {
-      zoom: 4,
-      center: position,
-      mapId: "DEMO_MAP_ID",
-    });
-
-    // The marker, positioned at Uluru
-    const marker = new AdvancedMarkerElement({
-      map: map,
-      position: position,
-      title: "Uluru",
-    });
-  }
-
-  initMap();
-
   return (
     <Stack
       sx={{
@@ -52,101 +20,121 @@ export default function map() {
       }}
     >
       <Box>{/* Map API here, Will work on later!. -T */}</Box>
+
       <Stack
-        direction="row"
-        spacing={3}
         sx={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <Box>
-          <Stack direction="row" spacing={3}>
-            <Stack
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                boxShadow: "0px 0px 20px 0px rgba(0, 0, 0, 0.05)",
-                padding: "24px",
-                borderRadius: "16px",
-              }}
-            >
-              <Typography
-                variant="h5"
+        <Stack
+          direction="row"
+          sx={{ alignItems: "center", marginBottom: "32px" }}
+          spacing={1}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            viewBox="0 0 22 22"
+            fill="none"
+          >
+            <path
+              d="M8.18645 2.60351C9.15292 -0.00834179 12.8471 -0.00833988 13.8136 2.60351L14.8427 5.38482C15.1466 6.20598 15.794 6.85341 16.6152 7.15727L19.3965 8.18645C22.0083 9.15292 22.0083 12.8471 19.3965 13.8136L16.6152 14.8427C15.794 15.1466 15.1466 15.794 14.8427 16.6152L13.8136 19.3965C12.8471 22.0083 9.15292 22.0083 8.18644 19.3965L7.15727 16.6152C6.85341 15.794 6.20598 15.1466 5.38482 14.8427L2.60351 13.8136C-0.00834179 12.8471 -0.00833988 9.15292 2.60351 8.18645L5.38482 7.15727C6.20598 6.85341 6.85341 6.20598 7.15727 5.38482L8.18645 2.60351Z"
+              fill="#18BA51"
+            />
+          </svg>
+          <Typography variant="h5">Хүргэлтийн бүс дэх хаягууд</Typography>
+        </Stack>
+        <Stack direction="row" spacing={3}>
+          <Box>
+            <Stack direction="row" spacing={3}>
+              <Stack
                 sx={{
-                  borderBottom: "1px solid var(--Brand-Green, #18BA51)",
-                  padding: "16px 0px",
+                  display: "flex",
+                  justifyContent: "center",
+                  boxShadow: "0px 0px 20px 0px rgba(0, 0, 0, 0.05)",
+                  padding: "24px",
+                  borderRadius: "16px",
                 }}
               >
-                А бүс
-              </Typography>
-              <Stack spacing={2} direction="row" sx={{ paddingTop: "16px" }}>
-                <Box>
-                  {locations.map((items) => (
-                    <Typography
-                      variant="h6"
-                      sx={{ paddingRight: "256px", fontWeight: 400 }}
-                    >
-                      {items}
-                    </Typography>
-                  ))}
-                </Box>
-                <Box>
-                  {locations.map((items) => (
-                    <Typography
-                      variant="h6"
-                      sx={{ paddingRight: "256px", fontWeight: 400 }}
-                    >
-                      {items}
-                    </Typography>
-                  ))}
-                </Box>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    borderBottom: "1px solid var(--Brand-Green, #18BA51)",
+                    padding: "16px 0px",
+                  }}
+                >
+                  А бүс
+                </Typography>
+                <Stack spacing={2} direction="row" sx={{ paddingTop: "16px" }}>
+                  <Box>
+                    {locations.map((items) => (
+                      <Typography
+                        variant="h6"
+                        sx={{ paddingRight: "256px", fontWeight: 400 }}
+                      >
+                        {items}
+                      </Typography>
+                    ))}
+                  </Box>
+                  <Box>
+                    {locations.map((items) => (
+                      <Typography
+                        variant="h6"
+                        sx={{ paddingRight: "256px", fontWeight: 400 }}
+                      >
+                        {items}
+                      </Typography>
+                    ))}
+                  </Box>
+                </Stack>
               </Stack>
-            </Stack>
-            <Stack
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                boxShadow: "0px 0px 20px 0px rgba(0, 0, 0, 0.05)",
-                padding: "24px",
-                borderRadius: "16px",
-              }}
-            >
-              <Typography
-                variant="h5"
+              <Stack
                 sx={{
-                  borderBottom: "1px solid var(--Brand-Green, #18BA51)",
-                  padding: "16px 0px",
+                  display: "flex",
+                  justifyContent: "center",
+                  boxShadow: "0px 0px 20px 0px rgba(0, 0, 0, 0.05)",
+                  padding: "24px",
+                  borderRadius: "16px",
                 }}
               >
-                Б бүс
-              </Typography>
-              <Stack spacing={2} direction="row" sx={{ paddingTop: "16px" }}>
-                <Box>
-                  {locations.map((items) => (
-                    <Typography
-                      variant="h6"
-                      sx={{ paddingRight: "256px", fontWeight: 400 }}
-                    >
-                      {items}
-                    </Typography>
-                  ))}
-                </Box>
-                <Box>
-                  {locations.map((items) => (
-                    <Typography
-                      variant="h6"
-                      sx={{ paddingRight: "256px", fontWeight: 400 }}
-                    >
-                      {items}
-                    </Typography>
-                  ))}
-                </Box>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    borderBottom: "1px solid var(--Brand-Green, #18BA51)",
+                    padding: "16px 0px",
+                  }}
+                >
+                  Б бүс
+                </Typography>
+                <Stack spacing={2} direction="row" sx={{ paddingTop: "16px" }}>
+                  <Box>
+                    {locations.map((items) => (
+                      <Typography
+                        variant="h6"
+                        sx={{ paddingRight: "256px", fontWeight: 400 }}
+                      >
+                        {items}
+                      </Typography>
+                    ))}
+                  </Box>
+                  <Box>
+                    {locations.map((items) => (
+                      <Typography
+                        variant="h6"
+                        sx={{ paddingRight: "256px", fontWeight: 400 }}
+                      >
+                        {items}
+                      </Typography>
+                    ))}
+                  </Box>
+                </Stack>
               </Stack>
             </Stack>
-          </Stack>
-        </Box>
+          </Box>
+        </Stack>
         <Box></Box>
       </Stack>
     </Stack>
