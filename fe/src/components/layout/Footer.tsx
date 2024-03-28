f154937d7b1e92fd7cca67a78049077972a5c2fa
7066 <festeringwound@proton.me>
2024-03-28 16:32:31 +0800

    removed header and footer in favor for newer one (See "Header" branch)

--- fe/src/components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-    return (
-      <div className="w-full h-[50vh] bg-[#18BA51] flex justify-center items-center">
-        <h1>This is a placeholder for the footer.</h1>
-      </div>
-    );
-  }
