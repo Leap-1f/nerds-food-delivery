ee34856a851a4cbc89f92aeaacb460a2adb302af
7066 <festeringwound@proton.me>
2024-03-28 17:18:40 +0800

    Reverted Changes made by me.

--- fe/src/pages/_document.js
+++ fe/src/pages/_document.js
@@ -1,7 +1,5 @@
 // pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { Header } from "../components/layout/Header.tsx";
-import { Footer } from "../components/layout/Footer.tsx";
 export default class MyDocument extends Document {
   render() {
     return (
@@ -19,17 +17,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
           />
         </Head>
-        <body
-          style={{
-            height: "100vh",
-            display: "flex",
-            justifyContent: "space-between",
-            flexDirection: "column",
-          }}
-        >
-          <Header />
+        <body>
           <Main />
-          <Footer />
           <NextScript />
         </body>
       </Html>
