fee6910da3b20fe0ea92f8b6991c88f31d4de0f0
Haliukaaa <haliuka.aqua@gmail.com>
2024-04-02 19:30:09 +0800

    connected mongoDB to backend

--- be/index.js
+++ be/index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import categoryConstant from "./constant/categoryConstant.js";
+import { Category } from "./model/Category.js";
 
 const port = 8080;
 const app = express();
@@ -6,9 +10,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const MONGO_CONNECTION_STRING =
+  "mongodb+srv://haliukaaqua:1aydxQMAiQDkIW2o@free.7gtcecr.mongodb.net/";
+
+mongoose
+  .connect(MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    Category.insertMany(categoryConstant.categories).then((docs) => {
+      console.log('Categories inserted successfully');
+      console.log(docs);
+    })
+    .catch((err) => {
+      console.error('Error inserting categories: ', err);
+    })
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB: ', err);
+  })
+
 app.get("/", (request, response) => {
-    response.send("Hello World!")
-})
+  response.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Your server is on on the port "http:localhost:${8080}"`);
