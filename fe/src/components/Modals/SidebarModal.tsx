b6bd89853692c8b4dc6fe0f74be349a3ba43b0fb
Myagmarsuren Buyanjargal <23LP9283@G401-CC0091.local>
2024-04-02 06:46:10 -0700

    SideBar duusaw ui

--- fe/src/components/Modals/SidebarModal.tsx
+++ fe/src/components/Modals/SidebarModal.tsx
@@ -5,11 +5,11 @@ import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { Stack } from "@mui/material";
 import { Typography } from "@mui/material";
 import { useState, KeyboardEvent, MouseEvent, Fragment } from "react";
-import ImageList from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import Image from "next/image";
-type Anchor = "top" | "left" | "bottom" | "right";
-import FoodImage from "../../../public/Image (1).png";
+import PizzaPhoto from "../../../public/Pizza.png";
+
+type Anchor = "right";
 
 export default function SideBarModal() {
   const [state, setState] = useState({
@@ -34,97 +34,149 @@ export default function SideBarModal() {
 
   const list = (anchor: Anchor) => (
     <Box
-      sx={{ width: 586 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100vh",
+      }}
     >
-      <Stack
-        direction={"row"}
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          px: 3,
-          pt: "26px",
-        }}
+      <Box
+        sx={{ width: 586 }}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
       >
-        <ArrowBackIosNewIcon
-          sx={{ width: "48px", height: "48px", px: "6px", py: "2px" }}
-        />
-        <Typography variant="h6">Таны сагс</Typography>
-        <Box sx={{ width: "48px" }}></Box>
-      </Stack>
-      <Box sx={{ px: 3, py: "27px" }}>
-        <Stack direction={"row"} sx={{ p: 2, gap: 2 }}>
-          <Box sx={{ width: "50%" }}>
-            <img src="asdf.png" alt="" style={{ height: "100%" }} />
-          </Box>
-          <Box
-            sx={{
-              gap: 1,
-              display: "flex",
-              flexDirection: "column",
-              width: "50%",
-            }}
-          >
-            <Stack direction={"row"}>
-              <Box sx={{ gap: "2px" }}>
-                <Typography variant="h6">Main Pizza</Typography>
-                <Box sx={{ color: "green" }}>34,800₮</Box>
-              </Box>
-              <Box sx={{ p: "5px" }}>
-                <CloseIcon />
-              </Box>
-            </Stack>
-            <Box>Хулуу, төмс, лууван , сонгино, цөцгийн тос, самрын үр </Box>
-            <Box sx={{ display: "flex", gap: 1 }}>
-              <Box
-                sx={{
-                  p: "30px",
-                  background: "#18BA51",
-                  borderRadius: "10px",
-                  width: "45px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  display: "flex",
-                  height: "40px",
-                  color: "white",
-                }}
-              >
-                -
-              </Box>
-              <Box
-                sx={{
-                  p: "30px",
-                  borderRadius: "10px",
-                  width: "45px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  display: "flex",
-                  height: "40px",
-                }}
-              >
-                1
-              </Box>
-              <Box
-                sx={{
-                  p: "30px",
-                  background: "#18BA51",
-                  borderRadius: "10px",
-                  width: "45px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  display: "flex",
-                  height: "40px",
-                  color: "white",
-                }}
-              >
-                +
+        <Stack
+          direction={"row"}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            px: 3,
+            pt: "26px",
+          }}
+        >
+          <ArrowBackIosNewIcon
+            sx={{ width: "48px", height: "48px", px: "6px", py: "2px" }}
+          />
+          <Typography variant="h6">Таны сагс</Typography>
+          <Box sx={{ width: "48px" }}></Box>
+        </Stack>
+
+        <Box sx={{ px: 3, py: "27px" }}>
+          <Stack direction={"row"} sx={{ p: 2, gap: 2 }}>
+            <Box sx={{ width: "50%" }}>
+              <Image
+                src={PizzaPhoto}
+                alt="PizzaPhoto"
+                style={{ height: "100%" }}
+              />
+            </Box>
+            <Box
+              sx={{
+                gap: 1,
+                display: "flex",
+                flexDirection: "column",
+                width: "50%",
+              }}
+            >
+              <Stack direction={"row"}>
+                <Box sx={{ gap: "2px" }}>
+                  <Typography variant="h6">Main Pizza</Typography>
+                  <Box sx={{ color: "green" }}>34,800₮</Box>
+                </Box>
+                <Box sx={{ p: "5px" }}>
+                  <CloseIcon />
+                </Box>
+              </Stack>
+              <Box>Хулуу, төмс, лууван , сонгино, цөцгийн тос, самрын үр </Box>
+              <Box sx={{ display: "flex", gap: 1 }}>
+                <Box
+                  sx={{
+                    p: "30px",
+                    background: "#18BA51",
+                    borderRadius: "10px",
+                    width: "45px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: "flex",
+                    height: "40px",
+                    color: "white",
+                  }}
+                >
+                  -
+                </Box>
+                <Box
+                  sx={{
+                    p: "30px",
+                    borderRadius: "10px",
+                    width: "45px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: "flex",
+                    height: "40px",
+                  }}
+                >
+                  1
+                </Box>
+                <Box
+                  sx={{
+                    p: "30px",
+                    background: "#18BA51",
+                    borderRadius: "10px",
+                    width: "45px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: "flex",
+                    height: "40px",
+                    color: "white",
+                  }}
+                >
+                  +
+                </Box>
               </Box>
             </Box>
+          </Stack>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          gap: "10px",
+          px: 4,
+          pt: "10px",
+          pb: 5,
+        }}
+      >
+        <Box
+          width={"50%"}
+          sx={{
+            display: "flex",
+
+            flexDirection: "column",
+          }}
+        >
+          <Box>Нийт төлөх дүн</Box>
+          <Typography variant="h6">34,800₮</Typography>
+        </Box>
+        <Box width={"50%"} sx={{ display: "flex", alignContent: "center" }}>
+          <Box
+            sx={{
+              bgcolor: "#18BA51",
+              px: 2,
+              py: 1,
+              borderRadius: "4px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+              color: "white",
+            }}
+          >
+            Захиалах
           </Box>
-        </Stack>
+        </Box>
       </Box>
     </Box>
   );
