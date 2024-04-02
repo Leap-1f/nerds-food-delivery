9933ab6042eb152000fc7480594ff8ce3c7b14ca
Haliukaaa <haliuka.aqua@gmail.com>
2024-04-02 18:22:53 +0800

    banner started, not finished

--- fe/src/pages/index.tsx
+++ fe/src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { FoodCart } from "@/components/ui/index";
+import { Banner } from "@/components/layout/Banner"; 
 
 export default function Home() {
   return (
     <>
+    <Banner></Banner>
       <FoodCart />
     </>
   );
