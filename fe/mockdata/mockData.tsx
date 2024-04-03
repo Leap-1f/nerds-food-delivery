6ac9d76bd0501af1d55812756defb8d16191fe72
Zaya Zoogii <zoogiizaya6@gmail.com>
2024-04-03 17:38:48 +0800

    menu zasvar

--- fe/mockdata/mockData.tsx
+++ fe/mockdata/mockData.tsx
@@ -4,7 +4,6 @@ export const data = [
     price: "14,000₮",
     dsprice: "16,800₮",
     discount: "20%",
-    category: "Breakfast",
     img: "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg",
   },
   {
@@ -12,7 +11,6 @@ export const data = [
     price: "4,000₮",
     dsprice: "5,800₮",
     discount: "20%",
-    category: "Soup",
     img: "https://redroserestaurant.softinfinitytechnology.com/wp-content/uploads/2021/10/from-the-us-russia-to-india-an-ice-cream-bowl-has-a-long-political-history.jpg",
   },
   {
@@ -20,8 +18,44 @@ export const data = [
     price: "24,000₮",
     dsprice: "28,800₮",
     discount: "20%",
-    category: "MainFood",
     img: "https://www.jigsawexplorer.com/puzzles/subjects/skillet-breakfast-436x300.jpg",
+  },
+
+  {
+    name: "Амттан",
+    price: "14,000₮",
+    dsprice: "16,800₮",
+    discount: "20%",
+    img: "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg",
+  },
+
+  {
+    price: "14,000₮",
+    dsprice: "16,800₮",
+    category: "Soup",
+    imgTwo:
+      "https://www.cubesnjuliennes.com/wp-content/uploads/2022/02/Homemade-Tomato-Soup-Recipe.jpg",
+  },
+  {
+    price: "14,000₮",
+    dsprice: "16,800₮",
+    category: "Breakfast",
+    imgTwo:
+      "https://images.immediate.co.uk/production/volatile/sites/2/2016/08/25097.jpg",
+  },
+  {
+    price: "14,000₮",
+    dsprice: "16,800₮",
+    category: "MainFood",
+    imgTow:
+      "https://www.foodandwine.com/thmb/j1vJdgrMdu64ElBpxMzmvqpyt5U=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/creamy-tomato-soup-buttery-croutons-hero-02-49b419d00f854db78838a79c8df9a23f.jpg",
+  },
+  {
+    price: "14,000₮",
+    dsprice: "16,800₮",
+    category: "Dessert",
+    imgTwo:
+      "https://myfoodstory.com/wp-content/uploads/2017/12/Homemade-Creamy-Vegetable-Soup-2-1.jpg",
   },
 
   {
@@ -29,36 +63,28 @@ export const data = [
     price: "14,000₮",
     dsprice: "16,800₮",
     discount: "20%",
-    category: "Dessert",
     img: "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg",
+  },
+  {
+    name: "Зайрмаг",
+    price: "4,000₮",
+    dsprice: "5,800₮",
+    discount: "20%",
+    img: "https://redroserestaurant.softinfinitytechnology.com/wp-content/uploads/2021/10/from-the-us-russia-to-india-an-ice-cream-bowl-has-a-long-political-history.jpg",
+  },
+  {
+    name: "Өглөөний хоол",
+    price: "24,000₮",
+    dsprice: "28,800₮",
+    discount: "20%",
+    img: "https://www.jigsawexplorer.com/puzzles/subjects/skillet-breakfast-436x300.jpg",
   },
 
   {
+    name: "Амттан",
     price: "14,000₮",
     dsprice: "16,800₮",
     discount: "20%",
-    category: "Soup",
-    img: "https://www.cubesnjuliennes.com/wp-content/uploads/2022/02/Homemade-Tomato-Soup-Recipe.jpg",
-  },
-  {
-    price: "14,000₮",
-    dsprice: "16,800₮",
-    discount: "20%",
-    category: "Soup",
-    img: "https://images.immediate.co.uk/production/volatile/sites/2/2016/08/25097.jpg",
-  },
-  {
-    price: "14,000₮",
-    dsprice: "16,800₮",
-    discount: "20%",
-    category: "Soup",
-    img: "https://www.foodandwine.com/thmb/j1vJdgrMdu64ElBpxMzmvqpyt5U=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/creamy-tomato-soup-buttery-croutons-hero-02-49b419d00f854db78838a79c8df9a23f.jpg",
-  },
-  {
-    price: "14,000₮",
-    dsprice: "16,800₮",
-    discount: "20%",
-    category: "Soup",
-    img: "https://myfoodstory.com/wp-content/uploads/2017/12/Homemade-Creamy-Vegetable-Soup-2-1.jpg",
+    img: "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg",
   },
 ];
--- fe/src/pages/menu.tsx
+++ fe/src/pages/menu.tsx
@@ -1,39 +1,30 @@
 import React from "react";
 import OrderModal from "@/components/Modals/OrderModal";
-import { FoodCart, InfoCard } from "@/components/ui/index";
+import { FoodCart, } from "@/components/ui/index";
 import { Box, Stack } from "@mui/material";
 
 export default function Menu() {
   const saleMeals = [
     {
-      img: "./star.svg",
-      name: "Хямдралтай",
-      seeAll: "Бүгд харах",
-      img2: "./seeAll.svg",
+     
       category: "Breakfast",
     },
     {
-      img: "./star.svg",
-      name: "Үндсэн хоол",
-      seeAll: "Бүгд харах",
-      img2: "./seeAll.svg",
+      
       category: "Soup",
     },
     {
-      img: "./star.svg",
-      name: "Салад ба зууш",
-      seeAll: "Бүгд харах",
-      img2: "./seeAll.svg",
+     
       category: "MainFood",
     },
     {
-      img: "./star.svg",
-      name: "Амттан",
-      seeAll: "Бүгд харах",
-      img2: "./seeAll.svg",
+    
       category: "Dessert",
     },
   ];
+  const category = "Dessert";
+  const filteredMeals = saleMeals.filter((meal) => meal.category === category);
+  console.log(filteredMeals);
   return (
     <Box>
       <Box
@@ -46,7 +37,6 @@ export default function Menu() {
           marginTop: "2%",
           display: "flex",
           justifyContent: "space-between",
-        
         }}
       >
         {saleMeals.map((el) => (
@@ -65,7 +55,6 @@ export default function Menu() {
               style={{
                 fontSize: "18",
                 fontWeight: "500",
-                lineHeight: "27",
                 color: "#000000",
               }}
             >
@@ -75,80 +64,6 @@ export default function Menu() {
         ))}
       </Box>
       <Box>
-        {saleMeals.map((el) => (
-          <Box
-            sx={{
-              height: "30%",
-              gap: "24px",
-              margin: "0px, auto",
-              marginTop: "2%",
-            }}
-          >
-            <Box
-              sx={{
-                maxWidth: "1250px",
-                marginLeft: "auto",
-                marginRight: "auto",
-                gap: "26px",
-              }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  maxWidth: "1250px",
-                  width: "1616px",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  marginTop: "2%",
-                }}
-              >
-                <img
-                  src={el.img}
-                  alt=""
-                  style={{ width: "32px", height: "32px" }}
-                />
-                <p
-                  style={{
-                    width: "85%",
-                    height: "10%",
-                    fontWeight: "700",
-                    fontSize: "22px",
-                    lineHeight: "33px",
-                    color: "#272727",
-                  }}
-                >
-                  {el.name}
-                </p>
-                <Box
-                  sx={{
-                    width: "8%",
-                    height: "10%",
-                    gap: "2px",
-                    display: "flex",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <p
-                    style={{
-                      fontWeight: "500",
-                      fontSize: "14px",
-                      lineHeight: "16px",
-                      alignItems: "center",
-                      color: "#18BA51",
-                    }}
-                  >
-                    {el.seeAll}
-                  </p>
-                  <img src={el.img2} alt="" />
-                </Box>
-              </Box>
-            </Box>
-            <Box sx={{ marginTop: "20px" }}>
-              <FoodCart></FoodCart>
-            </Box>
-          </Box>
-        ))}
       </Box>
       <Stack direction={"row"} justifyContent={"center"}>
         <OrderModal></OrderModal>
