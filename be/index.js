031a2aec23a9ca10618fe0922fbd251cd1dd0345
7066 <festeringwound@proton.me>
2024-04-03 18:45:02 +0800

    fester

--- be/index.js
+++ be/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import categoryConstant from "./constant/categoryConstant.js";
 import { Category } from "./model/Category.js";
+import { Food, foodSchema } from "./model/Food.js";
 
 const port = 8080;
 const app = express();
@@ -16,24 +17,46 @@ const MONGO_CONNECTION_STRING =
 mongoose
   .connect(MONGO_CONNECTION_STRING)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
 
-    Category.insertMany(categoryConstant.categories).then((docs) => {
-      console.log('Categories inserted successfully');
-      console.log(docs);
-    })
-    .catch((err) => {
-      console.error('Error inserting categories: ', err);
-    })
+    Category.insertMany(categoryConstant.categories)
+      .then((docs) => {
+        console.log("Categories inserted successfully");
+        console.log(docs);
+      })
+      .catch((err) => {
+        console.error("Error inserting categories: ", err);
+      });
   })
   .catch((err) => {
-    console.error('Error connecting to MongoDB: ', err);
-  })
+    console.error("Error connecting to MongoDB: ", err);
+  });
 
 app.get("/", (request, response) => {
   response.send("Hello World!");
 });
-
+app.post("/createFood", async (request, response) => {
+  const stringified = JSON.stringify(request.body);
+  const parsed = JSON.parse(stringified);
+  if (
+    parsed.name != "" ||
+    parsed.img != "" ||
+    parsed.ingredient != "" ||
+    parsed.price != 0
+  ) {
+    const createFood = Food.create({
+      name: parsed.name,
+      image: parsed.img,
+      ingredient: parsed.ingredient,
+      price: parsed.price,
+    });
+    response.status(200);
+    response.send("Created");
+  } else {
+    response.status(400);
+    response.send("Malformed Data");
+  }
+});
 app.listen(port, () => {
   console.log(`Your server is on on the port "http:localhost:${8080}"`);
 });
--- be/model/Food.js
+++ be/model/Food.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-export const foodSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    image: {type: String, required: true},
-    ingredient: {type: String, required: true},
-    price: {type: Number, required: true}, 
-}, {timestamps: true});
+export const foodSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    ingredient: { type: String, required: true },
+    price: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
 
-const Food = mongoose.model('Food', foodSchema);
+export const Food = mongoose.model("Food", foodSchema);
