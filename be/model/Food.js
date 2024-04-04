db7c8da9c4d32e1a1e9dc5082ceac4cbd920b2ed
Haliukaaa <haliuka.aqua@gmail.com>
2024-04-04 20:04:05 +0800

    hashed password and added a user

--- be/model/Food.js
+++ be/model/Food.js
@@ -5,7 +5,7 @@ const foodSchema = new mongoose.Schema({
     image: {type: String, required: true},
     ingredient: {type: String, required: true},
     price: {type: Number, required: true}, 
-    discountedPrice: {type: Number}
+    discountedPrice: {type: Number},
 }, {timestamps: true});
 
 export const Food = mongoose.model('Food', foodSchema);
